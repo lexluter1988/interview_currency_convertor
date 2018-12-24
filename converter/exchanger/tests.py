f46b1d05370c35fc8755d843ff6b7d8370423ae8
Alexey Suponin <asuponin@odin.com>
2018-12-24 19:22:19 +0700

    Fixes #19

--- converter/exchanger/tests.py
+++ converter/exchanger/tests.py
@@ -3,4 +3,39 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
+from .models import CurrencyData
+
+# python manage.py test exchanger.tests
+
+
+class BaseCurrencyDataTest(TestCase):
+    """ Test module for CurrencyData model """
+
+    def setUp(self):
+        CurrencyData.objects.create(
+            full_name='Czech Republic Koruna', short_name='CZK', exchange_rate=22.6887920000)
+        CurrencyData.objects.create(
+            full_name='Euro', short_name='EUR', exchange_rate=0.8770150000)
+        CurrencyData.objects.create(
+            full_name='Polish Zloty', short_name='PLN', exchange_rate=3.7598500000)
+        CurrencyData.objects.create(
+            full_name='United States Dollar', short_name='USD', exchange_rate=1)
+
+    def test_currencies_created(self):
+
+        czk = CurrencyData.objects.get(full_name='Czech Republic Koruna')
+        eur = CurrencyData.objects.get(full_name='Euro')
+        pln = CurrencyData.objects.get(full_name='Polish Zloty')
+        usd = CurrencyData.objects.get(full_name='United States Dollar')
+
+        self.assertEqual(
+            czk.short_name, 'CZK')
+        self.assertEqual(
+            eur.short_name, 'EUR')
+        self.assertEqual(
+            pln.short_name, 'PLN')
+        self.assertEqual(
+            usd.exchange_rate, 1)
+
+
+
--- converter/exchanger/views.py
+++ converter/exchanger/views.py
@@ -13,9 +13,16 @@ from serializers import CurrencyDataSerializer, InputSerializer
 from oexchangerateconnector import OpenExchangeRateConnector
 from config import OPEN_EXCHANGE_RATES_API_KEY, DAYS_OF_INACTIVITY, CURRENCY_PRECISION
 from django.utils import timezone
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
+@api_view(['GET'])
 def currency_rate_list(request):
+    """
+    Getting all Currencies with rates and latest update time.
+    """
     if request.method == 'GET':
         rates = CurrencyData.objects.all()
         # if no data about exchange rates, let's fill the DB
@@ -29,31 +36,43 @@ def currency_rate_list(request):
         rates = CurrencyData.objects.all()
 
         serializer = CurrencyDataSerializer(rates, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['POST'])
 @csrf_exempt
 def currency_convert(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = InputSerializer(data=data)
         if serializer.is_valid():
-            result = make_exchange(serializer.data)
-            return JsonResponse(result, status=201)
+            try:
+                result = make_exchange(serializer.data)
+            except CurrencyData.DoesNotExist:
+                Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(result)
 
-        return JsonResponse(serializer.errors, status=400)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET'])
 @csrf_exempt
 def currency_rate_detail(request, pk):
     try:
         currency = CurrencyData.objects.get(pk=pk)
     except CurrencyData.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = CurrencyDataSerializer(currency)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 def update_rates():
@@ -69,10 +88,15 @@ def make_exchange(data):
     currency_to = data['currency_to']
     amount = data['amount']
 
-    src_currency = CurrencyData.objects.get(short_name__exact=currency_from)
-    dst = CurrencyData.objects.get(short_name__exact=currency_to)
+    try:
+        src_currency = CurrencyData.objects.get(short_name__exact=currency_from)
+        dst_currency = CurrencyData.objects.get(short_name__exact=currency_to)
 
-    result = Decimal(amount) / src_currency.exchange_rate * dst.exchange_rate
+    except CurrencyData.DoesNotExist as e:
+        # TODO: make it cleaner
+        raise
+
+    result = Decimal(amount) / src_currency.exchange_rate * dst_currency.exchange_rate
 
     # need to reformat this
     data = {amount + ' ' + currency_from: str(round(result, CURRENCY_PRECISION)) + ' ' + currency_to}
