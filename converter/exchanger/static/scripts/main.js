f745c0f0d0433a9265fc001c99557e6fcd88c9af
Alexey Suponin <asuponin@odin.com>
2018-12-26 12:18:16 +0700

    Fixes #27, added unwrap of values

--- converter/exchanger/static/scripts/main.js
+++ converter/exchanger/static/scripts/main.js
@@ -1,10 +1,15 @@
 $(document).on('submit', '#currency-convert-form', function(event){
     event.preventDefault();
-    console.log("form submitted!")
+    console.log("form submitted!");
     convert_currency();
 });
 
 function convert_currency() {
-    console.log("converting currency!")
-    // console.log($('#amount').val())
+    var source = $('#id_source_currency option:selected').html();
+    var dest = $('#id_destination_currency option:selected').html();
+    var amount = $('#id_amount')[0].value;
+    console.log("converting currency!");
+    console.log(amount);
+    console.log(source);
+    console.log(dest);
 };
