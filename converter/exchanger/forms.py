26d09fdc700f25e4e82a87bc0f059d1616646d19
Alexey Suponin <asuponin@odin.com>
2018-12-26 12:00:47 +0700

    Fixes #27

--- converter/exchanger/forms.py
+++ converter/exchanger/forms.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django import forms
+from config import CURRENCY_FILTER, CURRENCY_PRECISION
 
 
 class CurrencyForm(forms.Form):
-    amount = forms.CharField(label='Amount to be converted', max_length=100)
+    source_currency = forms.ChoiceField(label='Currency From', choices=enumerate(tuple(CURRENCY_FILTER)), required=True)
+    amount = forms.DecimalField(label='Amount to be converted', max_digits=20, decimal_places=CURRENCY_PRECISION)
+    destination_currency = forms.ChoiceField(label='Currency To', choices=enumerate(tuple(CURRENCY_FILTER)), required=True)
+    result = forms.DecimalField(label='Result will be displayed here', max_digits=20, decimal_places=CURRENCY_PRECISION, required=False)
