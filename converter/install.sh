582ac9e21169b2c83b38f442cffe1d5ef10b27ad
Alexey Suponin <lexxsmith@gmail.com>
2018-12-26 19:21:14 +0700

    Delete install.sh

--- converter/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-virtualenv -p /usr/bin/python ./env
-. env/bin/activate
-pip install -r requirements.txt
-cd converter
-python manage.py test exchanger.tests -v 3
-python manage.py migrate
-python manage.py runserver
